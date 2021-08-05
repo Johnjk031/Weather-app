374d4f405b7b5a17824345e89df051bfdac3cbdd
Johnjk031 <john.juhlinkarlsson@gmail.com>
2021-08-05 12:22:24 +0200

    starting-point

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
+
+const [city, setCity] = useState('')
+const [data, setData] = useState({})
+
+let key = process.env.REACT_APP_KEY;
+
+let temp = '15'
+
+let api =  `https://api.openweathermap.org/data/2.5/`
+
+
+let getData = e => {
+  e.preventDefault()
+  console.log(city)
+  console.log(api)
+
+  fetch(`${api}weather?q=${city}&appid=${key}`)
+  .then(res => res.json())
+  .then(result => {
+    console.log(result)
+  })
+  
+console.log(process.env.REACT_APP_KEY);
+
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="App">
+  <main>
+    
+    <section className="search-box">
+    <form onSubmit={getData}>
+    <input 
+    type="text"
+    className="input-text"
+    onChange={e => setCity(e.target.value)}
+    />
+    <button type="submit">submit</button>
+    </form>
+    </section>
+
+  <section className="output">
+  <p>{city}</p>
+  <p>{temp}</p>
+  </section>
+
+
+  </main>
+    </section>
   );
 }
 
