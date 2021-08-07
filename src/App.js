88f70e81aa7d3fd95a4bca7dcc4b5fc24f943c92
Johnjk031 <john.juhlinkarlsson@gmail.com>
2021-08-07 22:05:45 +0200

    added styling

--- src/App.js
+++ src/App.js
@@ -7,18 +7,17 @@ const [city, setCity] = useState('')
 const [data, setData] = useState({})
 const [sectionOpen, setSectionOpen] = useState(false)
 
-
 let key = process.env.REACT_APP_KEY;
 
 const getDate = new Date();
 const currentDate = getDate.toDateString();
 
-let api =  `https://api.openweathermap.org/data/2.5/`
 
+
+let api =  `https://api.openweathermap.org/data/2.5/`
 
 let getData = e => {
   e.preventDefault()
- 
 
   fetch(`${api}weather?q=${city}&units=metric&appid=${key}`)
   .then(res => res.json())
@@ -27,34 +26,48 @@ let getData = e => {
     setData(result)
     setSectionOpen(true)
     console.log(result)
+   
   })
-  
 
 }
 
+
+
+
   return (
-    <section className="App">
-  <main>
+    <main>
+<section className="app">
     
+<p className="description">Check weather in:</p>
+
     <section className="search-box">
     <form onSubmit={getData}>
     
+    
+
     <input 
     type="text"
     className="input-text"
     onChange={e => setCity(e.target.value)}
     />
-    <button type="submit">search</button>
+    <button className="submit-btn" type="submit">search</button>
     
     </form>
     </section>
 
-  { data.cod === 200 ? (
+  { data.cod === 200 ?
+  (
 
   <section className={sectionOpen ? 'output' : 'hide'}>
   <p>{data.name}, {data.sys.country}</p>
+  
   <p>{currentDate}</p>
+
   <p>{Math.round(data.main.temp)}°</p>
+  <span>
+  <p className="weather-desc" style={{textTransform: 'capitalize'}}>{data.weather[0].description}</p>
+  <img src={`https://api.openweathermap.org/img/w/${data.weather[0].icon}`} alt="weather-icon"  />
+  </span>
   </section>
 
 ) :
@@ -63,14 +76,15 @@ let getData = e => {
 
  {sectionOpen && data.main === undefined ? (
   <section>
-    <p>{data.message}</p>
+    <p>{data.message} or country</p>
   </section>
 )
 : ''
 } 
   
-  </main>
     </section>
+    </main>
+
   );
 }
 
